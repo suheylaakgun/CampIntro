b3e6aa6d80409edea107db690091c66b09c6f116
suheylaakgun <a.suheyladogan@gmail.com>
2023-11-14 00:55:09 +0300

    3. gün ödevi

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Müşteri eklendi");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassMetotDemo;
+
+Customer customer = new Customer();
+customer.Id = 1;
+customer.Name = "Süheyla";
+
+CustomerManager customerManager = new CustomerManager();
+customerManager.Add(customer);
